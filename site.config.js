9c646c0f84d2bf9747e23aed2c5667b5450a970d
donghyun0215 <92701955+donghyun0215@users.noreply.github.com>
2026-05-18 18:25:10 +0800

    Update site.config.js

--- site.config.js
+++ site.config.js
@@ -24,7 +24,7 @@ const CONFIG = {
   },
 
   // CONFIG configration (required)
-  link: "https://donghyunkim-log.vercel.app",
+  link: "https://blog-donghyunkim.vercel.app",
   since: 2026, // If leave this empty, current year will be used.
   lang: "en-US", // ['en-US', 'zh-CN', 'zh-HK', 'zh-TW', 'ja-JP', 'es-ES', 'ko-KR']
   ogImageGenerateURL: "https://og-image-korean.vercel.app", // The link to generate OG image, don't end with a slash
