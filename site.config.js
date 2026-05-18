972bfaea252a80c70e5731de0e23c4c277175154
donghyun0215 <92701955+donghyun0215@users.noreply.github.com>
2026-05-18 18:05:57 +0800

    Initial commit

---
+++ site.config.js
@@ -0,0 +1,75 @@
+const CONFIG = {
+  // profile setting (required)
+  profile: {
+    name: "최정윤",
+    image: "/avatar.png", // If you want to create your own notion avatar, check out https://notion-avatar.vercel.app
+    role: "frontend developer",
+    bio: "알게 된 것과 깨달은 바를 요모조모 뜯어봅니다.",
+    email: "99choi0614@gmail.com",
+    linkedin: "c-jeongyyun",
+    github: "c-jeongyyun",
+    instagram: "",
+  },
+  projects: [
+    {
+      name: `journewn-log`,
+      href: "https://github.com/c-jeongyyun/journewn-log",
+    },
+  ],
+  // blog setting (required)
+  blog: {
+    title: "journewn-log",
+    description: "welcome to journewn-log!",
+    scheme: "dark", // 'light' | 'dark' | 'system'
+  },
+
+  // CONFIG configration (required)
+  link: "https://journewn-log.vercel.app",
+  since: 2026, // If leave this empty, current year will be used.
+  lang: "en-US", // ['en-US', 'zh-CN', 'zh-HK', 'zh-TW', 'ja-JP', 'es-ES', 'ko-KR']
+  ogImageGenerateURL: "https://og-image-korean.vercel.app", // The link to generate OG image, don't end with a slash
+
+  // notion configuration (required)
+  notionConfig: {
+    pageId: process.env.NOTION_PAGE_ID,
+  },
+
+  // plugin configuration (optional)
+  googleAnalytics: {
+    enable: false,
+    config: {
+      measurementId: process.env.NEXT_PUBLIC_GOOGLE_MEASUREMENT_ID || "",
+    },
+  },
+  googleSearchConsole: {
+    enable: false,
+    config: {
+      siteVerification: process.env.NEXT_PUBLIC_GOOGLE_SITE_VERIFICATION || "",
+    },
+  },
+  naverSearchAdvisor: {
+    enable: false,
+    config: {
+      siteVerification: process.env.NEXT_PUBLIC_NAVER_SITE_VERIFICATION || "",
+    },
+  },
+  utterances: {
+    enable: true,
+    config: {
+      repo: process.env.NEXT_PUBLIC_UTTERANCES_REPO || "",
+      "issue-term": "og:title",
+      label: "💬 Utterances",
+    },
+  },
+  cusdis: {
+    enable: false,
+    config: {
+      host: "https://cusdis.com",
+      appid: "", // Embed Code -> data-app-id value
+    },
+  },
+  isProd: process.env.VERCEL_ENV === "production", // distinguish between development and production environment (ref: https://vercel.com/docs/environment-variables#system-environment-variables)
+  revalidateTime: 21600 * 7, // revalidate time for [slug], index
+}
+
+module.exports = { CONFIG }
---
+++ src/routes/Feed/ProfileCard.tsx
@@ -0,0 +1,83 @@
+import styled from "@emotion/styled"
+import Image from "next/image"
+import React from "react"
+import { CONFIG } from "site.config"
+
+type Props = {}
+
+const ProfileCard: React.FC<Props> = () => {
+  return (
+    <StyledWrapper>
+      <div className="title">
+      </div>
+      <div className="content">
+        <div className="top">
+          <Image src={CONFIG.profile.image} alt="profile-avatar" width={180} height={180} style={{ objectFit: "cover" , borderRadius: "50%" }} />
+        </div>
+        <div className="mid">
+          <div className=" name">{CONFIG.profile.name}</div>
+          <div className="role">{CONFIG.profile.role}</div>
+          <div className="text-sm mb-2">{CONFIG.profile.bio}</div>
+        </div>
+      </div>
+    </StyledWrapper>
+  )
+}
+
+export default ProfileCard
+
+const StyledWrapper = styled.div`
+  > .title {
+    padding: 0.25rem;
+    margin-bottom: 0.75rem;
+  }
+  > .content {
+    margin-bottom: 2.25rem;
+    border-radius: 1rem;
+    width: 100%;
+    background-color: ${({ theme }) =>
+      theme.scheme === "light" ? "white" : theme.colors.gray4};
+    @media (min-width: 768px) {
+      padding: 1rem;
+    }
+    @media (min-width: 1024px) {
+      padding: 1rem;
+    }
+    .top {
+      position: relative;
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      // margin-bottom: 1rem;
+      width: 100%;
+      &:after {
+        content: "";
+        display: block;
+        padding-bottom: 100%;
+      }
+    }
+    .mid {
+      display: flex;
+      padding: 0.5rem;
+      flex-direction: column;
+      align-items: center;
+      .name {
+        font-size: 1.25rem;
+        line-height: 1.75rem;
+        font-style: italic;
+        font-weight: 700;
+      }
+      .role {
+        margin-bottom: 1rem;
+        font-size: 0.875rem;
+        line-height: 1.25rem;
+        color: ${({ theme }) => theme.colors.gray11};
+      }
+      .bio {
+        margin-bottom: 0.5rem;
+        font-size: 0.875rem;
+        line-height: 1.25rem;
+      }
+    }
+  }
+`
